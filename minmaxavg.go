b4f069a48eaac16496de9154b45b0492d71a6565
KIRUTHIKA <kirthi2001va@gmail.com>
2025-10-14 21:29:32 +0530

    GREEN: Implement code for smallest number

--- minmaxavg.go
+++ minmaxavg.go
@@ -14,10 +14,17 @@ func largest(num []int) int {
 }
 
 func smallest(num []int) int {
-	return 3
+	min := num[0]
+	for _, num := range num {
+		if num < min {
+			min = num
+		}
+	}
+	return min
 }
 
 func main() {
 	fmt.Println(largest([]int{5, 7, 3}))
 	fmt.Println(smallest([]int{5, 7, 3}))
+	fmt.Println(smallest([]int{20, 50, 10}))
 }
