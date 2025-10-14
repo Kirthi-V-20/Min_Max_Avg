a8e071288779814de44429e914265ca25d8b6a54
KIRUTHIKA <kirthi2001va@gmail.com>
2025-10-14 21:06:08 +0530

    GREEN: Adds code for smallest number

--- minmaxavg.go
+++ minmaxavg.go
@@ -13,6 +13,11 @@ func largest(num []int) int {
 	return max
 }
 
+func smallest(num []int) int {
+	return 3
+}
+
 func main() {
 	fmt.Println(largest([]int{5, 7, 3}))
+	fmt.Println(smallest([]int{5, 7, 3}))
 }
