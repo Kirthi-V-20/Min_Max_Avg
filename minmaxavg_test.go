cc92e80cb17574cf83ea4d1e06b571b1613761b0
KIRUTHIKA <kirthi2001va@gmail.com>
2025-10-14 20:37:40 +0530

    RED: Adds another test case for largest number

--- minmaxavg_test.go
+++ minmaxavg_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -9,6 +8,14 @@ func TestMax(t *testing.T) {
 	actual := largest([]int{5, 7, 3})
 	expected := 7
 	if actual != expected {
-		fmt.Errorf("Expected %d but got %d", expected, actual)
+		t.Errorf("Expected %d but got %d", expected, actual)
+	}
+}
+
+func TestMax1(t *testing.T) {
+	actual := largest([]int{20, 50, 10})
+	expected := 50
+	if actual != expected {
+		t.Errorf("Expected %d but got %d", expected, actual)
 	}
 }
